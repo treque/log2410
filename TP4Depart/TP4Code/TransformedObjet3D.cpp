5845045ad58a505e73c5b8022dbf1d9a433e588a
Huyen Trang Dinh <huyen-trang.dinh@polymtl.ca>
2018-11-14 19:08:46 -0500

    decorator success

--- TP4Depart/TP4Code/TransformedObjet3D.cpp
+++ TP4Depart/TP4Code/TransformedObjet3D.cpp
@@ -23,67 +23,74 @@ TransformedObjet3D::~TransformedObjet3D(){
 
 void TransformedObjet3D::addChild(const Objet3DAbs& obj3d){
 	// Deleguer a l'objet decore la tache d'ajouter un enfant
+	m_objet3d->addChild(obj3d);
 }
 
 
 Objet3DIterator TransformedObjet3D::begin()
 {
 	// A Completer...
-	return Objet3DBaseIterator();
+	return m_objet3d->begin();
 }
 
 
 Objet3DIterator_const TransformedObjet3D::cbegin() const {
 
 	// A Completer...
-	return Objet3DBaseIterator();
+	return m_objet3d->cbegin();
 }
 
 
 Objet3DIterator_const TransformedObjet3D::cend() const {
 
 	// A Completer...
-	return Objet3DBaseIterator();
+	return m_objet3d->cend();
 }
 
 
 Objet3DAbs* TransformedObjet3D::clone() const 
 {
 	// A Completer...
-	return nullptr;
+	return m_objet3d->clone();
 }
 
 
 Objet3DIterator TransformedObjet3D::end()
 {
 	// A Completer...
-	return Objet3DBaseIterator();
+	return m_objet3d->end();
 }
 
 
 Point3D TransformedObjet3D::getCenter() const 
 {
 	// A Completer...
-	return Point3D();
+	// je pense.. on decore! (translate)
+	return m_objet3d->getCenter() + m_translation;
 }
 
 
 size_t TransformedObjet3D::getNbParameters() const 
 {
 	// A Completer...
-	return 0;
+	return m_objet3d->getNbParameters();
 }
 
 
 PrimitiveParams TransformedObjet3D::getParameters() const 
 {
-	// A Completer...
-	return PrimitiveParams();
+	PrimitiveParams parameters = m_objet3d->getParameters();
+	for (int i = 0; i < getNbParameters(); i++) {
+		// on decore (scale)
+		parameters[i] *= m_scale;
+	}
+	return parameters;
 }
 
 
 void TransformedObjet3D::moveCenter(const Point3D& delta){
 	// A Completer...
+	m_objet3d->moveCenter(delta);
 }
 
 
@@ -91,39 +98,44 @@ void TransformedObjet3D::removeChild(Objet3DIterator_const obj3dIt)
 {
 	// Deleguer a l'objet decore la tache d'eliminer un enfant
 	// A Completer...
+	m_objet3d->removeChild(obj3dIt);
 }
 
 
 void TransformedObjet3D::setCenter(const Point3D& center){
 	// A Completer...
+	m_objet3d->setCenter(center);
 }
 
 
 void TransformedObjet3D::setParameter(size_t pIndex, float pValue)
 {
 	// A Completer...
+	m_objet3d->setParameter(pIndex, pValue);
 }
 
 float TransformedObjet3D::getScale() const
 {
 	// A Completer...
-	return 0.;
+	return m_scale;
 }
 
 void TransformedObjet3D::setScale(float scal)
 {
 	// A Completer...
+	m_scale = scal;
 }
 
 Point3D TransformedObjet3D::getTranslation() const
 {
 	// A Completer...
-	return Point3D();
+	return m_translation;
 }
 
 void TransformedObjet3D::setTranslation(const Point3D & translat)
 {
 	// A Completer...
+	m_translation = translat;
 }
 
 
